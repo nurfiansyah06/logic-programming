2ecb66848a9df4b5a9f5a9789a3cd24312363767
Muhamad Nur Arifansyah <nurfiansyah06@gmail.com>
2023-05-23 12:51:12 +0700

    add quadratic.go

---
+++ quadratic.go
@@ -0,0 +1,12 @@
+package logicprogramming
+
+func Quadratic(x1, x2 int) [3]int {
+	a := 1
+	b := -x1 - x2
+	c := x1 * x2
+
+	return [3]int{a, b, c}
+}
+
+// Quadratic Coefficients Solver in codewars
+
